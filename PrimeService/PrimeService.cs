6d14d66b0f6b2514783e407d9126ac4e6515c6a1
Berk <berkalgull@gmail.com>
2020-11-13 15:48:24 +0300

    Update PrimeService.cs

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -6,6 +6,7 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
+            int kullanilmayan_ve_uzun_bir_isme_sahip_degisken = 1;
             if (candidate < 2)
             {
                 return false;
